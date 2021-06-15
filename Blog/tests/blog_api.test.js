bdcbd18ac508a447a3fc28fe30202c2857981ef6
Panu Virta <panu.virta@student.hamk.fi>
2021-06-15 10:39:58 +0300

    4.11-4.12

--- Blog/tests/blog_api.test.js
+++ Blog/tests/blog_api.test.js
@@ -1,55 +1,94 @@
-const mongoose = require("mongoose");
-const supertest = require("supertest");
-const app = require("../app");
-const api = supertest(app);
-const helper = require("./test_helper");
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const helper = require('./test_helper')
 
-const Blog = require("../models/blog");
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
-  await Blog.deleteMany({});
+  await Blog.deleteMany({})
 
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
 
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
-});
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
 
-test("blogs are returned as json", async () => {
+test('blogs are returned as json', async () => {
   await api
-    .get("/api/blogs")
+    .get('/api/blogs')
     .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+    .expect('Content-Type', /application\/json/)
+})
 
-test("there are two blogs", async () => {
-  const response = await api.get("/api/blogs");
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(2);
-});
+  expect(response.body).toHaveLength(2)
+})
 
-test("a valid blog can be added", async () => {
+test('blog has property id', async () => {
+  const response = await helper.blogsInDb()
+  expect(response[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
   const newBlog = {
-    title: "Test3",
-    author: "Testman3",
-    url: "www.test3.com",
+    title: 'Test3',
+    author: 'Testman3',
+    url: 'www.test3.com',
     likes: 3,
-  };
+  }
 
   await api
-    .post("/api/blogs")
+    .post('/api/blogs')
     .send(newBlog)
     .expect(200)
-    .expect("Content-Type", /application\/json/);
+    .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  const contents = blogsAtEnd.map((r => r.title));
-  expect(contents).toContain("Test3");
-});
+  const contents = blogsAtEnd.map((r) => r.title)
+  expect(contents).toContain('Test3')
+})
+
+test('check if likes is missing, default to 0', async () => {
+  const newBlog = {
+    title: 'Test3',
+    author: 'Testman3',
+    url: 'www.test3.com',
+  }
+
+  const response = await api
+  .post('/api/blogs')
+  .send(newBlog)
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+
+expect(response.body.likes).toBeDefined()
+expect(response.body.likes).toBe(0)
+})
+
+test('fails with status code 400 if data invaild', async () => {
+  const newBlog = {
+    title: 'Testing',
+    author: 'Testman3',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
 
 afterAll(() => {
-  mongoose.connection.close();
-});
+  mongoose.connection.close()
+})
